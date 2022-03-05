1f9ab965014f816a298022328da047e1124e4517
Horváth Viktor <anyaviktor@gmail.com>
2022-03-05 20:21:58 +0100

    Grafikus felület fejlesztve, a játékos panel majdnem kész, listbox kell még Lecserélve a GBC metódus egy kompetensebbre ami több layout-ot is tud kezelni és hozzáadja a komponenseket a parenthez Több GBC beállítva

--- src/GUI/UI.java
+++ src/GUI/UI.java
@@ -10,28 +10,68 @@ public class UI extends JFrame
         setTitle("Játékprogram");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(true);
-        setLayout(new GridBagLayout());
 
-        JPanel jatekos_box = new JPanel();
+        setSize(new Dimension(700,500));
+        GridBagLayout layout = new  GridBagLayout();
+        GridBagConstraints constraints = new GridBagConstraints();
+
+        GridBagLayout jatekos_panel_layout =new GridBagLayout();
+
+        GridBagLayout NPC_panel_layout =new GridBagLayout();
+
+        GridBagLayout targy_panel_layout =new GridBagLayout();
+
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.weightx = constraints.weighty = 1f;
+        constraints.insets = new Insets(5,5,5,5);
+        setLayout(layout);
+
+        JPanel jatekos_box = new JPanel(jatekos_panel_layout);
         jatekos_box.setBorder(BorderFactory.createTitledBorder("Játékos"));
-        add(jatekos_box,GBCmaker(0,0));
+        addobjects(jatekos_box,this,layout,constraints,0,0,1,1);
+
+        JLabel jatekos_nev_label = new JLabel("Név:");
+        addobjects(jatekos_nev_label,jatekos_box,jatekos_panel_layout,constraints,0,0,1,1);
+
+        JTextField jatekos_nev_tb = new JTextField();
+        jatekos_nev_tb.setPreferredSize(new Dimension(140,25));
+        addobjects(jatekos_nev_tb,jatekos_box,jatekos_panel_layout,constraints,1,0,1,1);
+
+        JButton jatekos_felvesz_button = new JButton("Felvétel");
+        jatekos_felvesz_button.setPreferredSize(new Dimension(70,35));
+        addobjects(jatekos_felvesz_button,jatekos_box,jatekos_panel_layout,constraints,1,1,1,1);
+
+        JLabel jatekos_targy_felvetel_label = new JLabel("Tárgy felvétele:");
+        addobjects(jatekos_targy_felvetel_label,jatekos_box,jatekos_panel_layout,constraints,0,2,1,1);
+
+        JComboBox targy_ddl = new JComboBox(); //Tárgyakat kell felvenni bele
+        addobjects(targy_ddl,jatekos_box,jatekos_panel_layout,constraints,1,2,1,1);
+
 
         JPanel NPC_box = new JPanel();
         NPC_box.setBorder(BorderFactory.createTitledBorder("NPC"));
-        add(NPC_box,GBCmaker(1,0));
+        addobjects(NPC_box,this,layout,constraints,1,0,1,1);
+
+        JLabel NPC_nev_label = new JLabel("Név:");
+        addobjects(NPC_nev_label,NPC_box,NPC_panel_layout,constraints,0,0,1,1);
+
 
 
         JPanel targy_box = new JPanel();
         targy_box.setBorder(BorderFactory.createTitledBorder("Tárgy"));
-        add(targy_box,GBCmaker(2,0));
+        addobjects(targy_box,this,layout,constraints,2,0,1,1);
+
     }
-    private GridBagConstraints GBCmaker(int x, int y)
+    private void addobjects(Component component, Container container, GridBagLayout layout, GridBagConstraints gbc, int gridx, int gridy, int gridwidth, int gridheigth)
     {
-        GridBagConstraints constraint = new GridBagConstraints();
-        constraint.gridx = x;
-        constraint.gridy = y;
-        constraint.fill = GridBagConstraints.BOTH;
-        constraint.weightx = constraint.weighty = 1f;
-        return constraint;
+
+        gbc.gridx = gridx;
+        gbc.gridy = gridy;
+
+        gbc.gridwidth = gridwidth;
+        gbc.gridheight = gridheigth;
+
+        layout.setConstraints(component, gbc);
+        container.add(component);
     }
 }
