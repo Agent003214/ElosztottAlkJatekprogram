da126a8597811abc5c635f8e442dbf33f1e8ed08
Agent003214 <adamzsolt98@gmail.com>
2022-03-10 16:17:34 +0100

    Súly exception hoozáadva

--- src/Main.java
+++ src/Main.java
@@ -10,16 +10,6 @@ public class Main
 
     public static void main(String[] args)
     {
-        Jatekos teszt = new Jatekos("Jani");
-        Targy item=new Targy("Kurbli",5.1);
-        Targy item2=new Targy("Kormany",3);
-        /*teszt.addItem(item);
-        teszt.addItem(item2);*/
-        teszt.addItemToJatekos(item2);
-        teszt.getosszsuly();
-        System.out.println(teszt.getInventory());
-        //System.out.println(teszt.toString());
-
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception ignored) {
