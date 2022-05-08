2b46491e9e899af9faa99a3bb98aed881ed5f135
Horváth Viktor <anyaviktor@gmail.com>
2022-05-08 19:16:00 +0200

    JavaFX rendezés panel és a komponensei hozzáadva

--- src/GUI/JavaFXUI/JavaFXUI.java
+++ src/GUI/JavaFXUI/JavaFXUI.java
@@ -1,13 +1,11 @@
 package GUI.JavaFXUI;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -76,6 +74,46 @@ public class JavaFXUI extends Application
         Label NPC_label = new Label("NPC");
         gridPane.add(NPC_label,1,0);
         gridPane.add(NPC_panel,1,1);
+
+        //Tárgy panel és komponensei
+
+        GridPane targy_panel = new GridPane();
+        targy_panel.setBorder(black);
+        targy_panel.setPadding(new Insets(15));
+        targy_panel.setVgap(15);
+        targy_panel.setHgap(15);
+
+        Label targy_nev_label = new Label("Név:");
+        TextField targy_nev_tb = new TextField();
+        Label targy_suly_label = new Label("Súly:");
+        TextField targy_suly_tb = new TextField();
+        Button targy_felvetel_bt = new Button("Felvétel");
+
+
+        targy_panel.add(targy_nev_label,0,0);
+        targy_panel.add(targy_nev_tb,1,0);
+        targy_panel.add(targy_suly_label,0,1);
+        targy_panel.add(targy_suly_tb,1,1);
+        targy_panel.add(targy_felvetel_bt,1,2);
+
+        Label targy_label = new Label("Tárgy");
+        gridPane.add(targy_label,2,0);
+        gridPane.add(targy_panel,2,1);
+
+        //Rendezés panel és komponensei
+
+        String [] rendezesi_opciok = new String [] {"Név szerint","Súly szerint"};
+        GridPane rendezes_panel = new GridPane();
+        rendezes_panel.setVgap(15);
+        rendezes_panel.setHgap(15);
+
+        Label rendez_label = new Label("Rendezés:");
+        ComboBox<String> rendezes_ddl = new ComboBox<String>(FXCollections.observableArrayList(rendezesi_opciok));
+
+        rendezes_panel.add(rendez_label,0,0);
+        rendezes_panel.add(rendezes_ddl,1,0);
+
+        gridPane.add(rendezes_panel,0,2);
 
         Scene scene = new Scene(gridPane);
         primaryStage.setScene(scene);
